b827e921be9379671bc442e867d3b735b3d889db
iau676 <ibrahimuysal676@gmail.com>
2022-07-03 12:28:08 +0300

    set search table cell

---
+++ ios-dca-calculator/view/SearchTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SearchTableViewCell.swift
+//  ios-dca-calculator
+//
+//  Created by ibrahim uysal on 3.07.2022.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var assetNameLabel: UILabel!
+    @IBOutlet weak var assetSymbolLabel: UILabel!
+    @IBOutlet weak var assetTypeLabel: UILabel!
+    
+}
