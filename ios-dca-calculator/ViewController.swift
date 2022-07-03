b76fcc9bb5ad010b5c5855e8b2d2162b1fc733f9
iau676 <ibrahimuysal676@gmail.com>
2022-07-03 11:31:31 +0300

    Initial Commit

---
+++ ios-dca-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-dca-calculator
+//
+//  Created by ibrahim uysal on 3.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
