76d25860d5139a28a1c07dbeccf8c3447a3255f6
iau676 <ibrahimuysal676@gmail.com>
2022-07-04 19:28:31 +0300

    add SearchResults

---
+++ ios-dca-calculator/Model/SearchResults.swift
@@ -0,0 +1,35 @@
+//
+//  SearchResult.swift
+//  ios-dca-calculator
+//
+//  Created by ibrahim uysal on 4.07.2022.
+//
+
+import Foundation
+
+struct SearchResults: Decodable {
+    
+    let items: [SearchResult]
+    
+    enum codingKeys: String, CodingKey {
+        case items = "bestMatches"
+    }
+}
+
+struct SearchResult: Decodable {
+    
+    let symbol: String
+    let name: String
+    let type: String
+    let currency: String
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case symbol = "1. symbol"
+        case name = "2. name"
+        case type = "3. type"
+        case currency = "8. currency"
+        
+    }
+    
+}
