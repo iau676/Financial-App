827e51983a78c055ee3d1b7efa3da472e5355852
iau676 <ibrahimuysal676@gmail.com>
2022-07-07 20:01:51 +0300

    fix space bug

--- ios-dca-calculator/Service/APIService.swift
+++ ios-dca-calculator/Service/APIService.swift
@@ -10,15 +10,23 @@ import Combine
 
 struct APIService {
     
+    enum APIServiceError: Error {
+        case encoding
+        case badRequest
+    }
+    
     var API_KEY: String {
         return Key().keys.randomElement() ?? ""
     }
     
     func fetchSymbolsPublisher(keywords: String) -> AnyPublisher<SearchResults, Error> {
         
+        guard let keywords = keywords.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return Fail(error: APIServiceError.encoding).eraseToAnyPublisher() }
+        
         let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(API_KEY)"
         
-        let url = URL(string: urlString)!
+        guard let url = URL(string: urlString) else { return Fail(error: APIServiceError.badRequest).eraseToAnyPublisher() }
+        
         
         return URLSession.shared.dataTaskPublisher(for: url)
             .map({ $0.data })
