11dbab7d568cd66521f530bd7122b6cfce4976a0
iau676 <ibrahimuysal676@gmail.com>
2022-07-13 15:01:29 +0300

    handle dateSlider

--- ios-dca-calculator/Controller/CalculatorTableViewController.swift
+++ ios-dca-calculator/Controller/CalculatorTableViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class CalculatorTableViewController: UITableViewController {
     
@@ -16,15 +17,20 @@ class CalculatorTableViewController: UITableViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet var currencyLabels: [UILabel]!
     @IBOutlet weak var investmentAmountCurrencyLabel: UILabel!
+    @IBOutlet weak var dateSlider: UISlider!
     
     var asset: Asset?
     
-    private var initialDateOfInvestmentIndex: Int?
+    @Published private var initialDateOfInvestmentIndex: Int?
+    
+    private var subscribers = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
         setupTextFields()
+        setupDateSlider()
+        observeForm()
     }
     
     private func setupViews(){
@@ -36,10 +42,29 @@ class CalculatorTableViewController: UITableViewController {
         }
     }
     
-    private func setupTextFields() {
+    private func setupTextFields(){
         initialInvestmentAmountTextField.addDoneButton()
         monthlyDollarCostAveragingTextField.addDoneButton()
         initialDateOfInvestmentTextField.delegate = self
+    }
+    
+    private func setupDateSlider(){
+        if let count = asset?.timeSeriesMonthlyAdjusted.getMonthInfos().count {
+            let dateSliderCount = count.floatValue - 1
+            dateSlider.maximumValue = dateSliderCount
+        }
+    }
+    
+    private func observeForm(){
+        $initialDateOfInvestmentIndex.sink { [weak self] (index) in
+            guard let index = index else {return}
+            self?.dateSlider.value = index.floatValue
+            
+            if let dateString = self?.asset?.timeSeriesMonthlyAdjusted.getMonthInfos()[index].date.MMYYFormat {
+                self?.initialDateOfInvestmentTextField.text = dateString
+            }
+            
+        }.store(in: &subscribers)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -65,6 +90,10 @@ class CalculatorTableViewController: UITableViewController {
         }
     }
     
+    @IBAction func dateSliderDidChange(_ sender: UISlider) {
+        initialDateOfInvestmentIndex = Int(sender.value)
+    }
+    
 }
 
 
@@ -73,8 +102,9 @@ extension CalculatorTableViewController: UITextFieldDelegate {
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         if textField == initialDateOfInvestmentTextField {
             performSegue(withIdentifier: "showDateSelection", sender: asset?.timeSeriesMonthlyAdjusted)
+            return false
         }
-        return false
+        return true
     }
     
 }
---
+++ ios-dca-calculator/Extensions/Int+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Int+Extensions.swift
+//  ios-dca-calculator
+//
+//  Created by ibrahim uysal on 13.07.2022.
+//
+
+import Foundation
+
+extension Int {
+    
+    var floatValue: Float {
+        return Float(self)
+    }
+    
+}
