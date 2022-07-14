c8ef8fec8546710a517744d791c2aeaaff70c71a
iau676 <ibrahimuysal676@gmail.com>
2022-07-14 14:23:38 +0300

    observe textfields

--- ios-dca-calculator/Controller/CalculatorTableViewController.swift
+++ ios-dca-calculator/Controller/CalculatorTableViewController.swift
@@ -22,6 +22,8 @@ class CalculatorTableViewController: UITableViewController {
     var asset: Asset?
     
     @Published private var initialDateOfInvestmentIndex: Int?
+    @Published private var initialInvestmentAmount: Int?
+    @Published private var monthlyDollarCostAveragingAmount: Int?
     
     private var subscribers = Set<AnyCancellable>()
     
@@ -63,8 +65,24 @@ class CalculatorTableViewController: UITableViewController {
             if let dateString = self?.asset?.timeSeriesMonthlyAdjusted.getMonthInfos()[index].date.MMYYFormat {
                 self?.initialDateOfInvestmentTextField.text = dateString
             }
-            
         }.store(in: &subscribers)
+        
+        NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: initialInvestmentAmountTextField).compactMap({
+            ($0.object as? UITextField)?.text
+        }).sink { [weak self] (text) in
+            self?.initialInvestmentAmount = Int(text) ?? 0
+        }.store(in: &subscribers)
+        
+        NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: monthlyDollarCostAveragingTextField).compactMap({
+            ($0.object as? UITextField)?.text
+        }).sink { [weak self] (text) in
+            self?.monthlyDollarCostAveragingAmount = Int(text) ?? 0
+        }.store(in: &subscribers)
+        
+        Publishers.CombineLatest3($initialDateOfInvestmentIndex, $initialInvestmentAmount, $monthlyDollarCostAveragingAmount).sink { (initialDateOfInvestmentIndex, initialInvestmentAmount, monthlyDollarCostAveragingAmount) in
+            print("\(initialInvestmentAmount ?? 0), \(monthlyDollarCostAveragingAmount ?? 0), \(initialDateOfInvestmentIndex ?? 0)")
+        }.store(in: &subscribers)
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -95,7 +113,6 @@ class CalculatorTableViewController: UITableViewController {
     }
     
 }
-
 
 extension CalculatorTableViewController: UITextFieldDelegate {
     
