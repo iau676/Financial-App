25d918b9659673fb5aa332c2bc92fb0cb2fda862
iau676 <ibrahimuysal676@gmail.com>
2022-07-15 16:05:31 +0300

    add getNumberOfShares

--- ios-dca-calculator/Service/DCAService.swift
+++ ios-dca-calculator/Service/DCAService.swift
@@ -18,14 +18,15 @@ struct DCAService {
         
         let latestSharePrice = getLatestSharePrice(asset: asset)
         
-        let currentValue = getCurrentValue(numberOfShares: 100, latestSharePrice: latestSharePrice)
+        let numberOfShares = getNumberOfShares(asset: asset, initialInvestmentAmount: initialInvestmentAmount, monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount, initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
+        
+        let currentValue = getCurrentValue(numberOfShares: numberOfShares, latestSharePrice: latestSharePrice)
         
         return .init(currentValue: currentValue,
                      investmentAmount: investmentAmount,
                      gain: 0,
                      yield: 0,
                      annualReturn: 0)
-        
     }
     
     private func getInvestmentAmount(initialInvestmentAmount: Double,
@@ -45,6 +46,21 @@ struct DCAService {
     
     private func getLatestSharePrice(asset: Asset) -> Double {
         return asset.timeSeriesMonthlyAdjusted.getMonthInfos().first?.adjustedClose ?? 0
+    }
+    
+    private func getNumberOfShares(asset: Asset,
+                                   initialInvestmentAmount: Double,
+                                   monthlyDollarCostAveragingAmount: Double,
+                                   initialDateOfInvestmentIndex: Int) -> Double {
+        var totalShares = Double()
+        let initialInvestmentOpenPrice = asset.timeSeriesMonthlyAdjusted.getMonthInfos()[initialDateOfInvestmentIndex].adjustedOpen
+        let initialInvestmentShares = initialInvestmentAmount / initialInvestmentOpenPrice
+        totalShares += initialInvestmentShares
+        asset.timeSeriesMonthlyAdjusted.getMonthInfos().prefix(initialDateOfInvestmentIndex).forEach { (monthInfo) in
+            let dcaInvestmentShares = monthlyDollarCostAveragingAmount / monthInfo.adjustedOpen
+            totalShares += dcaInvestmentShares
+        }
+        return totalShares
     }
     
 }
