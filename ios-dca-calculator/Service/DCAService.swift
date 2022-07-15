7131e07be9bcd9723484c54f6f4fc84b303b61bd
iau676 <ibrahimuysal676@gmail.com>
2022-07-15 15:42:31 +0300

    add getInvestmentAmount

--- ios-dca-calculator/Service/DCAService.swift
+++ ios-dca-calculator/Service/DCAService.swift
@@ -13,12 +13,25 @@ struct DCAService {
                    monthlyDollarCostAveragingAmount: Double,
                    initialDateOfInvestmentIndex: Int) -> DCAResult {
         
+        let investmentAmount = getInvestmentAmount(initialInvestmentAmount: initialInvestmentAmount, monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount, initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
+        
         return .init(currentValue: 0,
-                     investmentAmount: 0,
+                     investmentAmount: investmentAmount,
                      gain: 0,
                      yield: 0,
                      annualReturn: 0)
         
+    }
+    
+    private func getInvestmentAmount(initialInvestmentAmount: Double,
+                           monthlyDollarCostAveragingAmount: Double,
+                           initialDateOfInvestmentIndex: Int) -> Double {
+        
+        var totalAmount = Double()
+        totalAmount += initialInvestmentAmount
+        let dollarCostAveragingAmount = initialDateOfInvestmentIndex.doubleValue * monthlyDollarCostAveragingAmount
+        totalAmount += dollarCostAveragingAmount
+        return totalAmount
     }
     
 }
