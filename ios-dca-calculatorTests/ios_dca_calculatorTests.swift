8b3b8af7d2896a77bb59b3a5106d25aa02766e1f
iau676 <ibrahimuysal676@gmail.com>
2022-07-17 12:54:29 +0300

    setup for unit tests

---
+++ ios-dca-calculatorTests/ios_dca_calculatorTests.swift
@@ -0,0 +1,38 @@
+//
+//  ios_dca_calculatorTests.swift
+//  ios-dca-calculatorTests
+//
+//  Created by ibrahim uysal on 17.07.2022.
+//
+
+import XCTest
+@testable import ios_dca_calculator
+
+class ios_dca_calculatorTests: XCTestCase {
+    
+    var sut: DCAService!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
